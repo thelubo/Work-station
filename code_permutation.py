9379c1244d69f17d385e374f5588f57b6a2f4153
thelubo <106333324+thelubo@users.noreply.github.com>
2022-09-03 15:14:47 +0300

    Add files via upload

---
+++ code_permutation.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 21 12:31:17 2022
+
+@author: thelu
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  4 17:54:58 2022
+
+@author: thelu
+"""
+
+import pandas as pd
+import argparse
+import itertools
+from itertools import permutations
+import math 
+import streamlit as st
+
+
+applicants = pd.read_csv ('D:\Applicants.csv')
+jobs = pd.read_csv ('D:\Jobs.csv')
+#print(jobs)
+#print("\n")
+#print(applicants)
+
+parser = argparse.ArgumentParser( description ='Viewing the contents of jobs.csv')
+parser.add_argument("-file", type = int, help="This will display the contents of the file depending on the number: 1-Jobs(displays Jobs)\n 2-Applicants(displays Applicants) \n 3-Both Jobs and Applicants(displays both Jobs and Applicants)")
+args = parser.parse_args()    
+
+if args.file == 1:
+    print("\n")
+    print(jobs)
+    print("\n")
+elif args.file == 2:
+    print("\n")
+    print(applicants)
+    print("\n")
+elif args.file == 3:
+    print("\n")
+    print(jobs)
+    print("\n")
+    print(applicants)
+    print("\n")
+
+#dict_from_csv_applicant = pd.read_csv('D:\Applicants.csv').to_dict()
+#print(dict_from_csv_applicant)
+
+#from itertools import permutations
+#a = permutations ([dict_from_csv_applicant])
+#for i in a:
+#    print(i)
+
+my_df_applicants= applicants.pivot(index='skill name',columns= 'applicant name',values = 'grade')
+#print(my_df_applicants)
+
+my_new_df_app = my_df_applicants.to_dict('dict')
+#print(my_new_df_app) 
+
+my_df_jobs = jobs.pivot(index='skill name',columns ='job name', values='grade')
+#print(my_df_jobs)
+
+my_new_df_jobs = my_df_jobs.to_dict('dict')
+#print(my_new_df_jobs)
+
+grade_job = my_new_df_jobs['Manager']['Learning']
+#print(grade_job)
+
+grade_applicant = my_new_df_app['Josh']['Communication']
+#print(grade_applicant)
+
+unique_applicants = set(my_new_df_app)
+unique_applist=list(unique_applicants)
+#print(unique_applist)
+
+unique_jobs = set(my_new_df_jobs)
+unique_joblist=list(unique_jobs)
+#print(unique_joblist)
+
+uniq_app_perm =  list(permutations(unique_applist))
+#print(uniq_app_perm)
+
+lst=[]
+for i in uniq_app_perm:
+    lst.append(tuple(zip(i,unique_joblist)))
+#print(lst)
+
+def fitting(lst,my_new_df_app,my_new_df_jobs):
+    res_val=0.0
+    for comp in my_new_df_app[unique_applist[0]]:
+        mult = my_new_df_app[unique_applist[0]]*my_new_df_jobs[unique_joblist[1]]
+        if math.isnun(mult):
+            mult=0.0
+        res_val=res_val + mult
+    return res_val
+
+print(fitting)
+
+def utility(lst,my_new_df_app,my_new_df_jobs):
+    res_util=0.0
+    for comb in lst:
+        res_util=res_util+ fitting(comb,my_new_df_app,my_new_df_jobs)
+    return res_util
+
+print(utility)
+
+def naive_approach(lst,my_new_df_app,my_new_df_jobs):
+    max=0.0
+    max_perm_element=(None,None)
+    for l in lst:
+        val=utility(l,my_new_df_app,my_new_df_jobs)
+        if val > max:
+            ress=val
+            ress_tpl=l
+    return [ress,ress_tpl]
+
+print(naive_approach)
+
